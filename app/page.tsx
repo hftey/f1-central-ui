566b6ce41250d10f3fcecbc6fa4dcde873314bdf
Raymond Tey <raymond.tey@gmail.com>
2024-06-07 10:31:07 +1000

    init commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,185 @@
+'use client';
+import clsx from "clsx";
 import Image from "next/image";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid, GridColDef, GridCellParams, GridFooterContainer, GridFooter } from '@mui/x-data-grid';
+import { useState, useEffect } from 'react'
+import type { InferGetStaticPropsType, GetStaticProps } from 'next'
 
-export default function Home() {
+const columns: GridColDef<(typeof rows)[number]>[] = [
+  {
+    field: 'ranking', headerName: 'Pos.', width: 50,type: 'number',
+    sortable: true,editable: false,
+    disableColumnMenu: true,
+    align: 'center',
+    headerClassName: 'text-black races',
+    cellClassName: 'relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert-0 bg-gradient-to-r from-slate-600 to-slate-700 text-gray-600 races text-white'
+  },
+  {
+    field: 'driver', headerName: 'Driver', width: 150,
+    sortable: false,editable: false,
+    disableColumnMenu: true,
+    headerClassName: 'text-black races',
+    cellClassName: 'text-white races',
+    renderCell: (params) => (
+      <div style={{backgroundImage: 'linear-gradient(to bottom right, '+ params.value.team_color + 'FF, '+ params.value.team_color + 'BB, '+ params.value.team_color + 'AA)'}}>
+        <img style={{height: '25px', display:'inline-block'}} src={params.value.team_logo}></img> {params.value.name}
+      </div>
+    )
+  },
+];
+
+let rows: any[] = [];
+
+const divStyle = {
+  display:'flex',
+  flexDirection:'column'
+};
+
+function CustomFooter () {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <GridFooterContainer sx={{paddingLeft: '15px'}}>
+      *sprint race
+      <GridFooter sx={{
+        border: 'none', // To delete double border.
+
+      }}>
+      </GridFooter>
+
+    </GridFooterContainer>
+  );
+}
+
+function loadDataHeader(data){
+  data.races.map((race) => {
+    columns.push({
+      field: race.tracks.name_short + race.id,
+      headerName: race.sprint ? race.tracks.name_short + "*" : race.tracks.name_short,
+      type: 'string',
+      width: 50,
+      editable: false,
+      sortable: true,
+      disableColumnMenu: true,
+      headerClassName: 'text-black races',
+      align: 'center',
+      valueGetter: (value: any) => {
+        return value;
+      },
+      renderCell: (params) => (
+        <p>{params.value.value}</p>
+      ),
+      sortComparator: (v1, v2) => {
+        let pos1 = v1.position == undefined ? 50 : v1.position;
+        let pos2 = v2.position == undefined ? 50 : v2.position;
+        pos1 = v1.position == 'RET' ? 49 : pos1;
+        pos2 = v2.position == 'RET' ? 49 : pos2;
+        return pos1 - pos2;
+      },
+      cellClassName: (params: GridCellParams<object>) => {
+        return clsx('relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert-0', {
+          'bg-gradient-to-r from-amber-300 via-amber-400 to-amber-300 text-gray-600 races': params.row[params.field].position == 1,
+          'bg-gradient-to-r from-amber-200 via-amber-300 to-amber-200 text-gray-600 races': params.row[params.field].position == 2,
+          'bg-gradient-to-r from-amber-100 via-amber-200 to-amber-100 text-gray-600 races': params.row[params.field].position == 3,
+          'bg-gradient-to-r from-lime-100 via-lime-200 to-lime-100 text-gray-600 races': params.row[params.field].position > 3 && (race.sprint ? params.row[params.field].position <= 8 : params.row[params.field].position <= 10),
+          'bg-gradient-to-r from-neutral-100 via-neutral-200 to-neutral-100 text-gray-600 races': (race.sprint ? params.row[params.field].position > 8 : params.row[params.field].position > 10) || params.row[params.field].position == '' || params.row[params.field].position == 'RET',
+          'bg-gradient-to-r from-neutral-50 via-neutral-100 to-neutral-50 text-gray-600 races': params.row[params.field].position == undefined,
+        })
+      }
+
+    })
+
+  });
+
+  columns.push({
+    field: 'points',
+    headerName: 'Points',
+    type: 'number',
+    width: 60,
+    editable: false,
+    sortable: false,
+    disableColumnMenu: true,
+    headerClassName: 'text-black',
+    align: 'center',
+    cellClassName: 'relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert-0 bg-gradient-to-r from-teal-600 to-teal-700 text-gray-600 races text-white'
+
+  })
+
+}
+
+let loaded = false;
+export default function Page() {
+  const [storeData, setStoreData] = useState([]);
+  const [byposition, setPosition] = useState(1);
+
+  useEffect(() => {
+    const url = process.env.NEXT_PUBLIC_API_URLxx+"/api/result/1/"+byposition;
+    fetch(url, {
+      method: "GET",
+      cache: "no-store"
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        if (!loaded) {
+          loadDataHeader(json);
+        }
+        setStoreData(json.result);
+        loaded = true;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [byposition]);
+
+  return (
+    <main className="flex min-h-screen flex-col items-left py-10 px-10">
+      <h2 className="text-4xl font-extrabold dark:text-white pb-10">Formula 1 2024 Result and Ranking</h2>
+      <div className="flex flex-row">
+        <div className="basis-1/12">
+          <div className="flex flex-row border-slate-100 rounded">
+            <div className="basis-1/4">
+              By:
+            </div>
+            <div className="basis-3/4">
+              <select className="row-start-1 col-start-1 px-2 text-left text-black rounded" onChange={(e) => {
+                setPosition(parseInt(e.target.value))
+              }}>
+                <option value={1}>Position</option>
+                <option value={0}>Points</option>
+              </select>
+            </div>
+          </div>
         </div>
       </div>
+      <Box sx={{ height: 'auto', width: '100%'}}>
+        {
+          loaded && <DataGrid
+            rows={storeData}
+            columns={columns}
+            rowHeight={30}
+            hideFooterPagination={true}
+            slots={{
+              footer: CustomFooter,
+            }}
+            sx={{
+              color: 'white',
+              fontSize: '0.8rem',
+              '& .MuiDataGrid-cell': {
+                padding: 0,
+              },
+              '& .MuiDataGrid-iconButtonContainer': {
+                position: 'absolute',
+                left: '0px',
+                top: '0px',
+              },
+            }}
+            disableRowSelectionOnClick
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          />
+        }
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      </Box>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    API_URL: 'http://f1central-asus'
+  },
+};
 
 export default nextConfig;
+
