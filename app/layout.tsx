7d1019c2b3f3b42acc2fd83c1cc13497caefde26
Raymond Tey <raymond.tey@gmail.com>
2024-07-03 15:19:55 +1000

    Include team compare table

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,24 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "F1 Statistics",
   description: "All your Formula 1 Statistics",
+  icons: {
+    icon: [
+      {
+        url: "/favicon.png",
+        href: "/favicon.png",
+      },
+    ],
+  },
+  openGraph: {
+    images: ['https://api.f1-central.net/images/share.jpg'],
+  },
 };
 
 export default function RootLayout({
@@ -17,6 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-VJNY0VBDE0" />
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Box from '@mui/material/Box';
 import { DataGrid, GridColDef, GridCellParams, GridFooterContainer, GridFooter } from '@mui/x-data-grid';
 import { useState, useEffect } from 'react'
 import type { InferGetStaticPropsType, GetStaticProps } from 'next'
+import { FacebookShare } from 'react-share-kit';
 
 const columns: GridColDef<(typeof rows)[number]>[] = [
   {
@@ -24,11 +25,112 @@ const columns: GridColDef<(typeof rows)[number]>[] = [
     cellClassName: 'text-white races',
     renderCell: (params) => (
       <div style={{backgroundImage: 'linear-gradient(to bottom right, '+ params.value.team_color + 'FF, '+ params.value.team_color + 'BB, '+ params.value.team_color + 'AA)'}}>
-        <img style={{height: '25px', display:'inline-block'}} src={params.value.team_logo}></img> {params.value.name}
+        <img alt={'Teams logo'} style={{height: '25px', display:'inline-block'}} src={params.value.team_logo}/> {params.value.name}
       </div>
     )
   },
 ];
+
+const column_stats: GridColDef<(typeof rows)[number]>[] = [
+  {
+    field: 'ranking', headerName: 'Pos.', width: 50,type: 'number',
+    sortable: true,editable: false,
+    disableColumnMenu: true,
+    align: 'center',
+    headerClassName: 'text-black races',
+    cellClassName: 'relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert-0 bg-gradient-to-r from-slate-600 to-slate-700 text-gray-600 races text-white'
+  },
+  {
+    field: 'drivers', headerName: 'Driver', width: 150,
+    sortable: false,editable: false,
+    disableColumnMenu: true,
+    headerClassName: 'text-black races',
+    cellClassName: 'text-white races',
+    renderCell: (params) => (
+      <div style={{backgroundImage: 'linear-gradient(to bottom right, '+ params.value.teams.color + 'FF, '+ params.value.teams.color + 'BB, '+ params.value.teams.color + 'AA)'}}>
+        <img alt={'Teams logo'} style={{height: '25px', display:'inline-block'}} src={params.value.teams.team_logo}/> {params.value.name}
+      </div>
+    )
+  },
+  {
+    field: 'best_start', headerName: 'Best Start', width: 85,
+    sortable: true,editable: false,type: 'number',
+    disableColumnMenu: true,align: 'center',
+    headerClassName: 'text-black races',
+    cellClassName: 'bg-gradient-to-r from-neutral-50 via-neutral-100 to-neutral-50 text-gray-600 races',
+  },
+  {
+    field: 'best_finish', headerName: 'Best Finish', width: 85,
+    sortable: true,editable: false,type: 'number',
+    disableColumnMenu: true,align: 'center',
+    headerClassName: 'text-black races',
+    cellClassName: 'bg-gradient-to-r from-neutral-50 via-neutral-100 to-neutral-50 text-gray-600 races',
+  },
+  {
+    field: 'avg_start', headerName: 'Avg Start', width: 75,
+    sortable: true,editable: false,type: 'number',
+    disableColumnMenu: true,align: 'center',
+    headerClassName: 'text-black races',
+    cellClassName: 'bg-gradient-to-r from-neutral-50 via-neutral-100 to-neutral-50 text-gray-600 races',
+  },
+  {
+    field: 'avg_end', headerName: 'Avg Finish', width: 75,
+    sortable: true,editable: false,type: 'number',
+    disableColumnMenu: true,align: 'center',
+    headerClassName: 'text-black races',
+    cellClassName: 'bg-gradient-to-r from-neutral-50 via-neutral-100 to-neutral-50 text-gray-600 races',
+  },
+  {
+    field: 'net_position', headerName: 'Pos. Gain/Lost', width: 100,
+    sortable: true,editable: false,type: 'number',
+    disableColumnMenu: true,align: 'center',
+    headerClassName: 'text-black races',
+    cellClassName: 'bg-gradient-to-r from-neutral-50 via-neutral-100 to-neutral-50 text-gray-600 races',
+  },
+  {
+    field: 'total_win', headerName: 'Total Win', width: 75,
+    sortable: true,editable: false,type: 'number',
+    disableColumnMenu: true,align: 'center',
+    headerClassName: 'text-black races',
+    cellClassName: 'bg-gradient-to-r from-neutral-50 via-neutral-100 to-neutral-50 text-gray-600 races',
+  },
+  {
+    field: 'total_podium', headerName: 'Total Podium', width: 90,
+    sortable: true,editable: false,type: 'number',
+    disableColumnMenu: true,align: 'center',
+    headerClassName: 'text-black races',
+    cellClassName: 'bg-gradient-to-r from-neutral-50 via-neutral-100 to-neutral-50 text-gray-600 races',
+  },
+  {
+    field: 'total_races', headerName: 'Total Races', width: 90,
+    sortable: true,editable: false,type: 'number',
+    disableColumnMenu: true,align: 'center',
+    headerClassName: 'text-black races',
+    cellClassName: 'bg-gradient-to-r from-neutral-50 via-neutral-100 to-neutral-50 text-gray-600 races',
+  },
+  {
+    field: 'total_laps', headerName: 'Total Laps', width: 75,
+    sortable: true,editable: false,type: 'number',
+    disableColumnMenu: true,align: 'center',
+    headerClassName: 'text-black races',
+    cellClassName: 'bg-gradient-to-r from-neutral-50 via-neutral-100 to-neutral-50 text-gray-600 races',
+  },
+  {
+    field: 'total_points',
+    headerName: 'Points',
+    type: 'number',
+    width: 60,
+    editable: false,
+    sortable: false,
+    disableColumnMenu: true,
+    headerClassName: 'text-black',
+    align: 'center',
+    cellClassName: 'relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert-0 bg-gradient-to-r from-teal-600 to-teal-700 text-gray-600 races text-white'
+
+  },
+
+];
+
 
 let rows: any[] = [];
 
@@ -41,6 +143,20 @@ function CustomFooter () {
   return (
     <GridFooterContainer sx={{paddingLeft: '15px'}}>
       *sprint race
+      <GridFooter sx={{
+        border: 'none', // To delete double border.
+
+      }}>
+      </GridFooter>
+
+    </GridFooterContainer>
+  );
+}
+
+function CustomFooterStats () {
+  return (
+    <GridFooterContainer sx={{paddingLeft: '15px'}}>
+      include sprint race results
       <GridFooter sx={{
         border: 'none', // To delete double border.
 
@@ -107,13 +223,18 @@ function loadDataHeader(data){
 
 }
 
+let shareUrl = 'https://www.f1-central.net';
+let titleToShare = 'F1 2024 Result and Ranking';
 let loaded = false;
+let teamDriverData:any = [];
 export default function Page() {
   const [storeData, setStoreData] = useState([]);
+  const [storeDataCompare, setStoreDataCompare] = useState([]);
+  const [storeDataStats, setStoreDataStats] = useState([]);
   const [byposition, setPosition] = useState(1);
 
   useEffect(() => {
-    const url = process.env.NEXT_PUBLIC_API_URLxx+"/api/result/1/"+byposition;
+    const url = process.env.NEXT_PUBLIC_API_URL+"/api/result/1/"+byposition;
     fetch(url, {
       method: "GET",
       cache: "no-store"
@@ -124,6 +245,8 @@ export default function Page() {
           loadDataHeader(json);
         }
         setStoreData(json.result);
+        setStoreDataCompare(json.team_driver_compare);
+        console.log(json.team_driver_compare);
         loaded = true;
       })
       .catch(function (error) {
@@ -131,9 +254,35 @@ export default function Page() {
       });
   }, [byposition]);
 
+
+
+  useEffect(() => {
+    const url = process.env.NEXT_PUBLIC_API_URL+"/api/stats/1";
+    fetch(url, {
+      method: "GET",
+      cache: "no-store"
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        setStoreDataStats(json.stats);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+
   return (
     <main className="flex min-h-screen flex-col items-left py-10 px-10">
-      <h2 className="text-4xl font-extrabold dark:text-white pb-10">Formula 1 2024 Result and Ranking</h2>
+      <div className="flex flex-row">
+        <div className="basis-3/4">
+          <h2 className="text-4xl font-extrabold dark:text-white pb-10" >Formula 1 2024 Result and Ranking</h2>
+        </div>
+        <div className="basis-1/4">
+          <FacebookShare url={shareUrl} quote={titleToShare} />
+
+        </div>
+      </div>
       <div className="flex flex-row">
         <div className="basis-1/12">
           <div className="flex flex-row border-slate-100 rounded">
@@ -151,7 +300,8 @@ export default function Page() {
           </div>
         </div>
       </div>
-      <Box sx={{ height: 'auto', width: '100%'}}>
+
+      <Box sx={{ height: 'auto', width: 'auto'}}>
         {
           loaded && <DataGrid
             rows={storeData}
@@ -162,6 +312,7 @@ export default function Page() {
               footer: CustomFooter,
             }}
             sx={{
+              width: 'auto !important',
               color: 'white',
               fontSize: '0.8rem',
               '& .MuiDataGrid-cell': {
@@ -169,7 +320,6 @@ export default function Page() {
               },
               '& .MuiDataGrid-iconButtonContainer': {
                 position: 'absolute',
-                left: '0px',
                 top: '0px',
               },
             }}
@@ -179,6 +329,99 @@ export default function Page() {
         }
 
       </Box>
+      <br/>
+      <Box sx={{ height: 'auto', width: '100%'}}>
+        {
+          loaded && <DataGrid
+            rows={storeDataStats}
+            columns={column_stats}
+            rowHeight={30}
+            hideFooterPagination={true}
+            slots={{
+              footer: CustomFooterStats,
+            }}
+            sx={{
+              color: 'white',
+              fontSize: '0.8rem',
+              '& .MuiDataGrid-cell': {
+                padding: 0,
+              }
+            }}
+            disableRowSelectionOnClick
+
+          />
+        }
+
+      </Box>
+
+      <br/>
+      <div className="flex flex-row" style={{flexWrap: 'wrap'}}>
+        <div className="basis-1/2">
+          <div className="flex flex-row">
+            <div className="basis-2/6 stat-header" style={{borderTopLeftRadius: '5px'}}>
+              <p>Driver</p>
+            </div>
+            <div className="basis-1/6 stat-header" style={{textAlign: 'center', borderLeft:'solid 1px silver'}}>
+              Start
+            </div>
+            <div className="basis-1/6 stat-header"></div>
+            <div className="basis-1/6 stat-header" style={{textAlign: 'center', borderLeft:'solid 1px silver'}}>
+              Finish
+            </div>
+            <div className="basis-1/6 stat-header"></div>
+          </div>
+        </div>
+        <div className="basis-1/2">
+          <div className="flex flex-row">
+            <div className="basis-2/6 stat-header">
+              Driver
+            </div>
+            <div className="basis-1/6 stat-header" style={{textAlign: 'center', borderLeft:'solid 1px silver'}}>
+              Start
+            </div>
+            <div className="basis-1/6 stat-header"></div>
+            <div className="basis-1/6 stat-header" style={{textAlign: 'center', borderLeft:'solid 1px silver'}}>
+              Finish
+            </div>
+            <div className="basis-1/6 stat-header"style={{borderTopRightRadius: '5px'}}></div>
+          </div>
+        </div>
+      {
+
+        //return console.log(storeDataCompare);
+        storeDataCompare.map((data:[], index) => {
+          return(
+            <div className="basis-1/2">
+            {data['driver'].map((driver_data:[], index2) => {
+                return (
+                  <div className="flex flex-row" sx={{ display: 'inline' }}>
+                    <div className="basis-2/6" style={{padding: '5px',display: 'inline', backgroundImage: 'linear-gradient(to bottom right, '+ data['team_detail'][0].team_color + 'FF, '+ data['team_detail'][0].team_color + 'BB, '+ data['team_detail'][0].team_color + 'AA)'}}>
+                      <img alt={'Teams logo'} style={{height: '25px', display:'inline-block'}} src={data['team_detail'][0].team_logo}/>
+                      {driver_data.name}
+                    </div>
+                    <div className="basis-1/6" style={{padding: '5px', textAlign: 'center', display: 'inline', backgroundImage: 'linear-gradient(to bottom right, '+ data['team_detail'][0].team_color + 'FF, '+ data['team_detail'][0].team_color + 'EE, '+ data['team_detail'][0].team_color + 'CC)'}}>
+                      {driver_data.start}
+                    </div>
+                    <div className="basis-1/6" style={{padding: '5px',textAlign: 'center', display: 'inline', backgroundImage: 'linear-gradient(to bottom right, '+ data['team_detail'][0].team_color + 'FF, '+ data['team_detail'][0].team_color + 'EE, '+ data['team_detail'][0].team_color + 'CC)'}}>
+                      {driver_data.start_percent} %
+                    </div>
+                    <div className="basis-1/6" style={{padding: '5px',textAlign: 'center', display: 'inline', backgroundImage: 'linear-gradient(to bottom right, '+ data['team_detail'][0].team_color + 'FF, '+ data['team_detail'][0].team_color + 'EE, '+ data['team_detail'][0].team_color + 'CC)'}}>
+                      {driver_data.end}
+                    </div>
+                    <div className="basis-1/6" style={{padding: '5px',textAlign: 'center', display: 'inline', backgroundImage: 'linear-gradient(to bottom right, '+ data['team_detail'][0].team_color + 'FF, '+ data['team_detail'][0].team_color + 'EE, '+ data['team_detail'][0].team_color + 'CC)'}}>
+                      {driver_data.end_percent} %
+                    </div>
+                  </div>
+            )
+            })}
+            </div>
+          )
+        })
+      }
+      </div>
+      <br />
+      <br />
+      <br />
 
     </main>
   );
